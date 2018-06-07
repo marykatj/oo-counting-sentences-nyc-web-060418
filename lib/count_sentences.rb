183d35e46a38decbef240d68804f0e7518a57553
Mary-Kate James <marykatj@gmail.com>
2018-06-07 00:33:34 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") ? true : false
+    self.end_with?(".") ? true : false
   end
 
   def question?
-    if self.end_with?("?") ? true : false
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-    if self.end_with?("!") ? true : false
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
